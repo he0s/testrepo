1913e7c0622f6b3f5704108b18628590a709d93f
Andrey Nikitin <anikitin@mirantis.com>
2015-08-03 13:55:59 +0300

    Added a code to print data from the /etc/python_jenkins/foo_bar.config

--- hello-world.py
+++ hello-world.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
+import os
+
 def main():
+
+    path = '/etc/python_jenkins/foo_bar.config'
+    fl = open(path, 'r')
+    data = fl.read()
+    fl.close()
+    print data
 
     print 'Hello world!'
 
